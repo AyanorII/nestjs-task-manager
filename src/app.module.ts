c118e316e21b4029626232d0ee6264dadc2f09cb
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-09-13 19:01:07 +0900

    add auth module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { config } from 'ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config), TasksModule],
+  imports: [TypeOrmModule.forRoot(config), TasksModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
