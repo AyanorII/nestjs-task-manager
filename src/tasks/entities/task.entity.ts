8a913f7d07b9101521221ed16ea5be61f9c0cd6a
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-09-13 18:29:13 +0900

    add task entity

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,17 @@
-export class Task {}
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { TaskStatus } from '../tasks-status.enum';
+
+@Entity()
+export class Task extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: TaskStatus;
+}
