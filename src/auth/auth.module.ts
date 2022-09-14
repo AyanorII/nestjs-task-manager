9ac82ae26ae17891dbf3f62d2a58d67ed874b9b6
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-09-14 12:47:28 +0900

    add passport and jwt modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'topSecret51', // Obviously, should be a better secret.
+      signOptions: {
+        expiresIn: 3600, // Expires in one hour.
+      },
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
