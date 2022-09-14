e995869f36304070104bea8ec9fd3edff8575fd3
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-09-14 15:52:27 +0900

    add access token JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,28 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './entities/user.entity';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return User.createUser(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDto;
     const user = await User.findOne({ where: { username } });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'success';
+      const payload: JwtPayload = { username };
+      const accessToken: string = await this.jwtService.sign(payload);
+      return { accessToken };
     } else {
       throw new UnauthorizedException('Please check your login credentials.');
     }
